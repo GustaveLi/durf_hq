80eab40a007e6eb933355810a3f9314cac3bd721
Gustave Li <hl4212@nyu.edu>
2021-07-19 18:44:41 +0800

    Dimensionality reduction for triad coordinate files

--- projects/Gustave_Li/Main_program/Dimensionality_reduction.py
+++ projects/Gustave_Li/Main_program/Dimensionality_reduction.py
@@ -11,48 +11,40 @@ args = parser.parse_args()
 method_name=args.method
 
 # Specify target directory for file reading and writing
-results_dir = '/xspace/hl4212/durf_hq/projects/Gustave_Li/Main_program/results/Newer_version'
+results_dir = '/xspace/hl4212/durf_hq/projects/Gustave_Li/Main_program/results/dimensionality_reduction'
 
-# Load discriptors for dimensionality reduction
-descriptor_path = f'{results_dir}/descriptors_arr.npy'
-d_array = np.load(descriptor_path)
-
-if method_name.lower() == 'pca':
-    pca = PCA(n_components=2)
-    result_arr = pca.fit_transform(d_array)
-    
-elif method_name.lower() == 'kpca_poly':
-    kpca = KernelPCA(n_components=2, kernel='poly')
-    result_arr = kpca.fit_transform(d_array)
-    
-elif method_name.lower() == 'kpca_rbf':
-    kpca = KernelPCA(n_components=2, kernel='rbf')
-    result_arr = kpca.fit_transform(d_array)
-    
-elif method_name.lower() == 'tsne_standard':
-    tsne_std = TSNE()
-    result_arr = tsne_std.fit_transform(d_array)
-    
-elif method_name.lower() == 'tsne_optimized':
-    tsne_optm = TSNE(n_components=2, perplexity=30, learning_rate=(len(d_array)//12), \
-                     init='pca')
-    result_arr = tsne_optm.fit_transform(d_array)
-    
-elif method_name.lower() == 'tsne_xyz':
-    # Load the xyz file on disk
+# Load arrays for dimensionality reduction
+if 'xyz' in method_name:
     xyz_dir = '/gpfsnyu/scratch/hl4212'
     xyz_path = f'{xyz_dir}/xyz_aligned.npy'
-    xyz = np.load(xyz_path)
+    arr = np.load(xyz_path)
+
+else:
+    descriptor_path = f'{results_dir}/descriptors_arr.npy'
+    arr = np.load(descriptor_path)
     
-    xyz_pipeline = Pipeline([
-        #('pca', PCA(n_components=50)),
-        ('tSNE_opt', TSNE(n_components=2,
-                          learning_rate=(len(xyz)//12), 
-                          perplexity=100)),
-        ])
+# Start dimreduct
+if method_name.lower() == 'pca' or method_name.lower() == 'pca_xyz':
+    pca = PCA(n_components=2)
+    result_arr = pca.fit_transform(arr)
     
-    result_arr = xyz_pipeline.fit_transform(xyz)
+elif method_name.lower() == 'kpca_poly' or method_name.lower() == 'kpca_poly_xyz':
+    kpca = KernelPCA(n_components=2, kernel='poly')
+    result_arr = kpca.fit_transform(arr)
     
+elif method_name.lower() == 'kpca_rbf' or method_name.lower() == 'kpca_rbf_xyz':
+    kpca = KernelPCA(n_components=2, kernel='rbf')
+    result_arr = kpca.fit_transform(arr)
+    
+elif method_name.lower() == 'tsne_standard' or method_name.lower() == 'tsne_standard_xyz':
+    tsne_std = TSNE()
+    result_arr = tsne_std.fit_transform(arr)
+    
+elif method_name.lower() == 'tsne_optimized' or method_name.lower() == 'tsne_optimized_xyz':
+    tsne_optm = TSNE(n_components=2, perplexity=30, learning_rate=(len(d_array)//12), \
+                     init='pca')
+    result_arr = tsne_optm.fit_transform(arr)
+     
 # Save the data back to disk
 result_path = f'{results_dir}/dimreduct_{method_name}.npy'
 np.save(result_path, result_arr)
